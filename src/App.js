e93ca5cf2219721129a9be89bf4d10a3ab840cc9
Kotauror <zygmunt.justyna@gmail.com>
2021-06-07 16:46:11 +0100

    Add luxon code.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { DateTime } from "luxon"
 
 function App() {
   return (
@@ -16,10 +17,24 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          {formatDate("2021-05-06T10:00")}
         </a>
       </header>
     </div>
   );
 }
+
+export function formatDate(dateTime) {
+  return DateTime.fromJSDate(parseReflexUtcDateTime(dateTime), { zone: "utc" })
+    .setZone("America/Chicago")
+    .toFormat("MM/dd/yyyy H:mma ZZZZ")
+}
+
+
+function parseReflexUtcDateTime(dateTime) {
+  const b = dateTime.split(/\D/)
+  return new Date(Date.UTC(b[0], b[1] - 1, b[2], b[3], b[4]))
+}
+
 
 export default App;
